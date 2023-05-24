380e96a0cb1205a8c1c0e13a5a9d9d9d000be740
zurson <matu.jurek@gmail.com>
2023-05-24 16:24:52 +0200

    isActive field is now used

--- Server/src/Debug.java
+++ Server/src/Debug.java
@@ -2,9 +2,15 @@ public class Debug {
 
     private boolean isActive;
 
-    public Debug(boolean active){ this.isActive = active; }
+    public Debug(boolean active) {
+        this.isActive = active;
+    }
 
-    public void message(String message){
+    public void message(String message) {
+
+        if (!isActive)
+            return;
+
         System.out.print(Color.CYAN);
         System.out.println("(Debug) " + message);
         System.out.print(Color.RESET);
